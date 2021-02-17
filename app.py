62425df7e83e12abfba589c8a58e885f745ffb0e
Siddharth Goyal <50585327+SidGoyal2014@users.noreply.github.com>
2021-02-17 21:10:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,15 @@ with open('config.json', 'r') as c:
 app = Flask(__name__)
 
 app.secret_key = "super_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://eurek8d7_common:common#123?@md-in-30.webhostbox.net:2083/eurek8d7_techblogflask'
 app.config["UPLOAD_FOLDER"] = params_1["upload_location"]
+
+SQLALCHEMY_POOL_RECYCLE=90
+
+# app.config['SQLALCHEMY_POOL_RECYCLE'] = app.config['SQLALCHEMY_POOL_TIMEOUT'] - 1
+
+# app.config.from_object(os.environ['APP_SETTINGS'])
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #  Config the flask mail
 app.config.update(
@@ -32,12 +40,7 @@ app.config.update(
 
 mail = Mail(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://eurek8d7_common:common#123?@md-in-30.webhostbox.net:2083/eurek8d7_techblogflask'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/tech blog- flask'
-
-# SQLALCHEMY_POOL_RECYCLE=90
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 90
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/tech blog- flask'
 db = SQLAlchemy(app)
 
 class Contacts(db.Model):
@@ -66,7 +69,7 @@ class Admin_panel_credentials(db.Model):
 def home():
     posts = Posts.query.filter_by().all()
     last = math.ceil(len(posts)/int(params_1["no_of_posts"]))
-    print("last : ",last)
+    # print("last : ",last)
     page = request.args.get("page")
 
     if(not str(page).isnumeric()):
@@ -76,7 +79,7 @@ def home():
 
     posts = posts[((page-1)*int(params_1["no_of_posts"])) : ((page-1)*int(params_1["no_of_posts"])+int(params_1["no_of_posts"]))]
 
-    print("print : ",page)
+    # print("print : ",page)
 
     if(page == 1 and last == 1):
         prev = "#"
@@ -92,11 +95,6 @@ def home():
         next = "/?page="+str(page+1)
 
     return render_template("index.html", params=params, posts=posts, prev=prev, next=next)
-    
-    """
-    posts = Posts.query.filter_by().all()[0:params_1["no_of_posts"]]
-    return render_template('index.html',params=params, posts=posts)
-    """
 
 @app.route("/post/<string:post_slug>", methods=['GET'])
 def post_route(post_slug):
@@ -112,22 +110,72 @@ def add():
     if ("user" in session and session["user"]!=None):
         print("Session is there")
         if(request.method == "POST"):
-            # print("title : ",title)
-            # print("subheading : ",subheading)
             title = request.form.get("title")
             subheading = request.form.get("subheading")
             slug = request.form.get("slug")
             content = request.form.get("content")
             date = datetime.now()
-            img_filename = "netaji"
 
-            print("title : ",title)
-            print("subheading : ",subheading)
+            post2 = Posts.query.filter_by(slug = slug).all()
 
-            entry = Posts(title=title, Subheading=subheading, slug=slug, content=content,img_filename=img_filename, date=date)
+            if(len(post2) > 0):
+                return render_template("notify.html", params=params)
+        
+            # Save the file
+            f = request.files["file1"]
 
-            db.session.add(entry)
-            db.session.commit()
+            flag = True
+
+            if(f.filename == ""):
+                flag = False  # No file uploaded    
+                img_filename = "netaji"
+
+                print("title : ",title)
+                print("subheading : ",subheading)
+
+                # Create the entry
+                entry = Posts(title=title, Subheading=subheading, slug=slug, content=content,img_filename=img_filename, date=date)
+
+                # Add the session in the database
+                db.session.add(entry)
+                # Commit the changes to the database
+                db.session.commit()
+            else:
+                flag = True  # File uploaded
+
+                f.save(os.path.join(app.config["UPLOAD_FOLDER"], secure_filename(f.filename)))
+
+                img_filename = "netaji"
+
+                print("title : ",title)
+                print("subheading : ",subheading)
+
+                # Create the entry
+                entry = Posts(title=title, Subheading=subheading, slug=slug, content=content,img_filename=img_filename, date=date)
+
+                # Add the session in the database
+                db.session.add(entry)
+                # Commit the changes to the database
+                db.session.commit()
+
+                posts = Posts.query.filter_by().all()
+                x = len(posts)
+                # print("x : ",x)
+                sno1 = posts[x-1].sno
+                # print("sno1 : ",sno1)
+                # print("f.name : ",f.name)
+                # print("Filename : ",f.filename)
+
+                extension = f.filename.split(".")
+
+                os.rename((params_1["upload_location"] + "\\" +f.filename) , (params_1["upload_location"] + "\\" + str(sno1) + "." + str(extension[1])))
+
+                name_of_new_image = str(sno1) + "."  + extension[1]
+
+                post1 = Posts.query.filter_by(sno = sno1).first()
+                post1.img_filename = name_of_new_image
+                db.session.commit()
+
         return render_template("add.html", params=params)
     return render_template("login.html", params=params)
 
@@ -141,61 +189,118 @@ def edit(sno):
             subheading = request.form.get('subheading')
             slug = request.form.get('slug')
             content = request.form.get('content')
-            # print("title: ",title)
-            # print("subheading: ",subheading)
-            """
-            if (sno == '0'):
-                post = Posts(title=title, subheading=subheading, slug=slug, content=content)
-                db.session.add(post)
-                db.session.commit()
-            """
-            # else:
-            # print("sno : ",sno)
-            # print("title : ",title)
-            post = Posts.query.filter_by(sno = sno).first()
-            post.title = title
-            post.Subheading = subheading
-            post.slug = slug
-            post.content = content
+
+            # Load the row from the database
+            post1 = Posts.query.filter_by(sno = sno).first()
+
+            # Store the current row values in auxiliary memory
+            prev_sno = post1.sno
+            prev_title = post1.title
+            prev_subheading = post1.Subheading
+            prev_slug = post1.slug
+            prev_content = post1.content
+            prev_img_filename = post1.img_filename
+            prev_date = post1.date
+
+            # Delete the current row from the database
+            post2 = Posts.query.filter_by(sno=sno).first()
+            db.session.delete(post2)
             db.session.commit()
-        post = Posts.query.filter_by(sno=sno).first()
-        return render_template("edit.html", sno=sno, params=params,post=post)
+
+            # Now, we have to check if the slug is unique or not
+            post = Posts.query.filter_by(slug = slug).all()
+
+            # Check if the current row slug is unique or not
+            if(len(post) > 0):
+                # print("NO")
+                # If not
+                entry = Posts(title = prev_title, Subheading=prev_subheading, slug=prev_slug, content=prev_content, img_filename = prev_img_filename, date=prev_date)
+                db.session.add(entry)
+                db.session.commit()
+
+                # Change the filename & reflect the changes in the database
+                post3 = Posts.query.filter_by().all()
+
+                x = len(post3)
+
+                new_sno = post3[x-1].sno
+
+                extension = prev_img_filename.split(".")
+
+                os.rename((params_1["upload_location"] + "\\" +prev_img_filename) , (params_1["upload_location"] + "\\" + str(new_sno) + "." + str(extension[1])))
+
+                # Reflect the changes in the database
+
+                post3[x-1].img_filename = str(new_sno) + "." + str(extension[1])
+
+                db.session.commit()
+
+                return render_template("edit.html", params=params, post=post1)
+            else:
+                # Add the new post in the database
+                entry = Posts(title=title, Subheading=subheading, slug=slug, content=content,img_filename=prev_img_filename, date=datetime.now())
+                db.session.add(entry)
+                db.session.commit()
+
+                # Change the filename & reflect the changes in the database
+                post3 = Posts.query.filter_by().all()
+
+                x = len(post3)
+
+                new_sno = post3[x-1].sno
+
+                extension = prev_img_filename.split(".")
+
+                os.rename((params_1["upload_location"] + "\\" +prev_img_filename) , (params_1["upload_location"] + "\\" + str(new_sno) + "." + str(extension[1])))
+
+                post3[x-1].img_filename = str(new_sno) + "." + str(extension[1])
+
+                db.session.commit()
+            
+                # Return to Dashboard
+                post4 = Posts.query.filter_by().all()
+
+                return render_template("dashboard.html", params=params,posts=post4)
+
+        post3 = Posts.query.filter_by(sno=sno).first()
+        return render_template("edit.html", sno=sno, params=params,post=post3)
     else:
-        print("The session is not there")
-        return render_template("login.html", params=params)
+        return redirect("/login")
 
-@app.route("/dashboard", methods=["GET", "POST"])
-def dashboard():
-
-    posts = Posts.query.all()
-
-    if "user" in session:
-        return render_template("dashboard.html", params=params,posts=posts)
-
-    # the below if block is for letting the admin in the dashboard after succesful login
-    if request.method == "POST":
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if("user" in session):
+        return redirect("/dashboard")
+    if(request.method == "POST"):
         username = request.form.get("username")
         password = request.form.get("password")
 
-        print("username ",username)
-        print("password ",password)
-
         # Making the object of the the table credetials
         credentials = Admin_panel_credentials.query.filter_by().all()
-        # CORRECT print(credentials[0].username)
 
         for i in range(0,len(credentials)):
             if((username in credentials[i].username) and (password in credentials[i].password)):
+                print("Password matched")
                 session["user"] = username
-                return render_template("dashboard.html",params=params,posts=posts)
+                return redirect("/dashboard")
+                # return render_template("dashboard.html",params=params,posts=posts)
+    return render_template("login.html", params=params)
 
-    return render_template('login.html',params=params)
+@app.route("/dashboard", methods=["GET", "POST"])
+def dashboard():
+    posts = Posts.query.all()
+    # To check if the admin is already logged in
+    if ("user" in session):
+        posts = Posts.query.all()
+        # posts = Posts.query.all()
+        return render_template("dashboard.html", params=params,posts=posts)
+    return redirect("/login")
 
 @app.route("/logout")
 def logout():
     session["user"] = None
     session.pop("user")
-    return redirect("/dashboard")
+    return redirect("/login")
 
 @app.route("/uploader", methods=["GET","POST"])
 def uploader():
